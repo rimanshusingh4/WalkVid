cb80281268b7aec9b52049cd668c86e82958f1ca
Rimanshu Singh <rimanshusingh4@gmail.com>
2024-07-02 01:10:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='h-screen w-full text-9xl text-zinc-50 bg-black flex justify-center items-center'>
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
